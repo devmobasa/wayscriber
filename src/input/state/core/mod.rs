23b580347a61a3e4e5d66d365c853aff4c6e4b9f
devmobasa <4170275+devmobasa@users.noreply.github.com>
2025-11-05 20:03:41 +0100

    refactor input state files

--- src/input/state/core/mod.rs
+++ src/input/state/core/mod.rs
@@ -1,8 +1,12 @@
 mod base;
+mod board;
+mod dirty;
+mod highlight_controls;
 mod index;
 mod menus;
 mod properties;
 mod selection;
+mod selection_actions;
 
 pub use base::{DrawingState, InputState};
 pub use menus::{ContextMenuEntry, ContextMenuKind, ContextMenuState};
